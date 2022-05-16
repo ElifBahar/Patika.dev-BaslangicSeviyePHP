15523656350dc8af12b472a2f519472ef6b5381f
ElifBahar <elif.har94@gmail.com>
2022-05-16 12:42:16 +0300

    add if else conditionals js

---
+++ JAVASCRIPT/workingWithDecisionMaking-and-Conditions/js/02-conditionals-ifElse.js
@@ -0,0 +1,18 @@
+// ******************** If Else Koşul Yapısı ********************
+
+console.log(" *********************** IF ELSE KOŞUL YAPISI SCRIPTLERI BAŞLANGIÇ **********************");
+
+
+
+// *** eşitse -> == ***
+
+let username = prompt("Kullanıcı adınızı giriniz : ");
+
+// *** eğer kullanıcı bilgisi varsa ekrana yazı yazdırma ***
+if( username.length ){
+    console.log(`Kullanıcı bilginiz : ${username}`);
+}else{
+    console.log("Bilgi Yok");
+}
+
+console.log(" *********************** IF ELSE KOŞUL YAPISI SCRIPTLERI BİTİŞ **********************");
